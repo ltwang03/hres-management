50ef678959d7da28c09cf2235e2581d0670e0395
lequang03 <lethanhquang147@gmail.com>
2023-03-10 12:18:37 +0700

    add DBHelper and SyncServices

---
+++ app/src/main/java/edu/huflit/hres_management/Database/DBHelper.java
@@ -0,0 +1,21 @@
+package edu.huflit.hres_management.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+//
+//public class DBHelper extends SQLiteOpenHelper {
+//
+//
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//
+//    }
+//}
