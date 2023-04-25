ab9f64f2a75fd366f7998d2d7d91ac94f9850a6e
dangphuong2003 <114977514+dangphuong2003@users.noreply.github.com>
2023-04-25 09:34:24 +0700

    2/3 darkmode

--- app/src/main/java/edu/huflit/hres_management/ProfileActivity.java
+++ app/src/main/java/edu/huflit/hres_management/ProfileActivity.java
@@ -1,16 +1,27 @@
 package edu.huflit.hres_management;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 import edu.huflit.hres_management.Fragment.BottomBarFragment;
 import edu.huflit.hres_management.Fragment.InfoProfileFragment;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    SwitchCompat mswitchMode;
+    boolean nightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +30,28 @@ public class ProfileActivity extends AppCompatActivity {
         Fragment infoProfile = new InfoProfileFragment();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.layout_profile, infoProfile).commit();
+
+        mswitchMode = findViewById(R.id.switchMode);
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMode = sharedPreferences.getBoolean("nightMode",false);
+        if(nightMode){
+            mswitchMode.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        mswitchMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(nightMode){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode",false);
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode",true);
+                }
+                editor.apply();
+            }
+        });
     }
 }
