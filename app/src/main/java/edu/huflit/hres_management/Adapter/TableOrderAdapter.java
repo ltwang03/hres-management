87072cdd0f9f0e4bafb0c81929f347822a788d27
renzisheree <mapneverdie1@gmail.com>
2023-04-26 18:48:55 +0700

    update layout order

--- app/src/main/java/edu/huflit/hres_management/Adapter/TableOrderAdapter.java
+++ app/src/main/java/edu/huflit/hres_management/Adapter/TableOrderAdapter.java
@@ -13,28 +13,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
-import edu.huflit.hres_management.BookingTableActivity;
 import edu.huflit.hres_management.Database.DBHelper;
 import edu.huflit.hres_management.Model.TableOrder;
-import edu.huflit.hres_management.OrderFoodActivity;
 import edu.huflit.hres_management.R;
 
 public class TableOrderAdapter  extends RecyclerView.Adapter<TableOrderAdapter.TableOrderViewHolder>
@@ -143,8 +133,8 @@ public class TableOrderAdapter  extends RecyclerView.Adapter<TableOrderAdapter.T
         mbtnOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(mContext, OrderFoodActivity.class);
-                mContext.startActivity(i);
+//                Intent i = new Intent(mContext, OrderFoodActivity.class);
+//                mContext.startActivity(i);
             }
         });
 
