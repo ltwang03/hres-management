ffa6f7408a6fb3ee139f9eed021293c893b8e5fd
phuong <puongpp0712@gmail.com>
2023-11-22 22:06:50 +0700

    edit customer

--- app/src/main/java/edu/huflit/hres_management/Adapter/CustomerAdapter.java
+++ app/src/main/java/edu/huflit/hres_management/Adapter/CustomerAdapter.java
@@ -1,21 +1,32 @@
 package edu.huflit.hres_management.Adapter;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.IllegalFormatCodePointException;
 import java.util.List;
 
+import edu.huflit.hres_management.ListCustomerActivity;
 import edu.huflit.hres_management.Model.Customer;
 import edu.huflit.hres_management.R;
 
 public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder> {
     private Context mContext;
+    private Button mbtn_cancelCustomer, mbtn_updateCustomer;
     private List<Customer> mListCustomer;
 
     public CustomerAdapter(Context mContext) {
@@ -40,6 +51,34 @@ public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.Custom
         if(customer == null)
             return;
         holder.tvName.setText(customer.getName());
+        holder.mbtn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        View form = LayoutInflater.from(mContext).inflate(R.layout.dialog_edit_customer, null);
+        builder.setView(form);
+        AlertDialog dialog = builder.create();
+        mbtn_cancelCustomer = form.findViewById(R.id.btn_cancelCustomer);
+        mbtn_updateCustomer = form.findViewById(R.id.btn_updateCustomer);
+        mbtn_cancelCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        mbtn_updateCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        dialog.show();
+
     }
 
     @Override
@@ -51,10 +90,17 @@ public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.Custom
     }
     public class CustomerViewHolder extends RecyclerView.ViewHolder{
         private TextView tvName;
+        private ImageView mbtn_edit, mbtn_delete;
+
+
 
         public CustomerViewHolder(@NonNull View itemView) {
             super(itemView);
             tvName = itemView.findViewById(R.id.name_customer);
+            mbtn_delete = itemView.findViewById(R.id.delete_customer);
+            mbtn_edit = itemView.findViewById(R.id.ic_edit_customer);
+            mbtn_cancelCustomer = itemView.findViewById(R.id.btn_cancelCustomer);
+            mbtn_updateCustomer = itemView.findViewById(R.id.btn_updateCustomer);
         }
     }
 }
